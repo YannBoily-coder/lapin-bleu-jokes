7ee57865b0a30799ba0e2ac40a96788340aa6fe2
YannBoily <yann.boily@gmail.com>
2026-06-08 04:36:05 +0200

    Update index.php
    add php
    
    Signed-off-by: YannBoily <yann.boily@gmail.com>

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+<?php
+$quotes = json_decode(file_get_contents("jcvd_quotes.json"), true)["quotes"];
+$random = $quotes[array_rand($quotes)];
+
+echo "<h1>🐇 Lapin Bleu Jokes – Mode JCVD</h1>";
+echo "<p>$random</p>";
+?>
 
